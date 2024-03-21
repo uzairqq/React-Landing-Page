5388f0e33d507b5fbba1ee2a677f891bcc5534de
Uzair Iqbal <uzair.qq@outlook.com>
2024-03-21 20:00:10 +0500

    Simple NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Hello World!!!</>;
-}
+const App = () => {
+  return (
+    <div>
+      <div>
+        <nav>
+          <div className="logoNavbar">
+            <img src="./assets/img/brand_logo.png" alt="logo" />
+          </div>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button>Login</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
