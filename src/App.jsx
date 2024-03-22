e58cc12101b213fd25f3a55fe2bd2cb5bbf7a6e5
Uzair Iqbal <uzair.qq@outlook.com>
2024-03-22 16:58:09 +0500

    NavBar With NavBar Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
+import "./App.css";
 const App = () => {
   return (
     <div>
-      <div>
-        <nav>
-          <div className="logoNavbar">
-            <img src="./assets/img/brand_logo.png" alt="logo" />
-          </div>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
+      <nav>
+        <div className="logoNavbar">
+          <img src="/src/assets/img/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
         <button>Login</button>
-      </div>
+      </nav>
     </div>
   );
 };
